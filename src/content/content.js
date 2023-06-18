126853f3e7988057ac8561f1e0bd3f473c6437b0
Danila Kulinkovich <danek200326@gmail.com>
2023-06-18 23:56:24 +0300

    feat(ESDU-00): gavnocod, but okay

--- src/content/content.js
+++ src/content/content.js
@@ -2,36 +2,56 @@ import './content.css'
 import createJoin from './join/join';
 import createDiscover from './discover/discover';
 import createSign from './signin/sign';
-import {createHeader} from '../header/header'
 const join = createJoin();
 const discover = createDiscover();
 const sign = createSign();
-const { discovera, joina, signupa  } = createHeader();
-let clickedLink = "discover";
+export const createContent = (discovera, joina, signupa) => {
+  function setClickedLink(link) {
+    content.innerHTML = '';
+    clickedLink = link;
+  }
+  function updateContent() {
+    content.innerHTML = '';
+    var htmlElement = document.documentElement;
 
-function setClickedLink(link) {
-  clickedLink = link;
-}
-discovera.addEventListener('click', function() {
-   console.log(5)
-  });
-  joina.addEventListener('click', function() {
-    setClickedLink('join');
-  });
-  signupa.addEventListener('click', function() {
-    setClickedLink('sign');
-  });
-const createContent = () => {
+    if (clickedLink === "discover") {
+      content.appendChild(discover);
+      htmlElement.style.backgroundImage = "none";
+    } else if (clickedLink === "join") {
+      content.appendChild(join);
+      htmlElement.style.backgroundImage = "url('../../assets/images/background-page-landing.png')";
+    } else if (clickedLink === "sign") {
+      content.appendChild(sign);
+      htmlElement.style.backgroundImage = "url('../../assets/images/background-page-sign-up.png')";
+   
+    }
+    
+  const lefttext = document.createElement('p');
+  lefttext.classList.add('content__text');
+  lefttext.textContent = 'Wlodzimierz Simon © 2022';
+  content.appendChild(lefttext);
+  }
+  let clickedLink = "discover";
+  discovera.addEventListener('click', function() {
+    setClickedLink('discover');
+    console.log(clickedLink)
+    updateContent();
+   });
+   joina.addEventListener('click', function() {
+     setClickedLink('join');
+     console.log(clickedLink)
+     updateContent();
+   });
+   signupa.addEventListener('click', function() {
+     setClickedLink('sign');
+     console.log(clickedLink)
+     updateContent();
+   });
     const content = document.createElement('div');
     content.classList.add('content');
-console.log(clickedLink)
-
-    const lefttext = document.createElement('p');
-    lefttext.classList.add('content__text');
-    lefttext.textContent = 'Wlodzimierz Simon © 2022';
-
-    content.appendChild(lefttext);
+   
+    updateContent();
     return content;
 };
 
-export default createContent;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import {createHeader} from './header/header';
-import createContent from './content/content';
+import {createContent} from './content/content';
 import createFooter from './footer/footer';
 const body = document.querySelector('body');
-const { header } = createHeader();
-const content = createContent();
+const { header, discovera, joina, signupa } = createHeader();
+const content = createContent(discovera, joina, signupa);
 const footer = createFooter();
+  
 body.appendChild(header);
 body.appendChild(content);
 body.appendChild(footer);
